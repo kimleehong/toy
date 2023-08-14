e319e12a415a922022254b998d37e5b33af22cf0
kimleehong <klh1523@naver.com>
2023-08-14 15:13:00 +0900

    first

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function main_slide() {
+  var swiper = new Swiper(".banner", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+}
+
+$(function () {
+  main_slide();
+});
